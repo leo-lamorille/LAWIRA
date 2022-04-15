7ba4f0e88fc34577878c123ab36e7300d41d5ade
leo lamorille <lamorilleleo@gmail.com>
2022-04-15 13:57:45 +0200

    ➕ ; add material ui icon

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import MenuIcon from '@material-ui/icons/Menu';
+import LocalGroceryStoreOutlinedIcon from '@mui/icons-material/LocalGroceryStoreOutlined';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <body>
+          <div className="container">
+              <MenuIcon />
+              <LocalGroceryStoreOutlinedIcon className="test"/>
+          </div>
+      </body>
     </div>
   );
 }
