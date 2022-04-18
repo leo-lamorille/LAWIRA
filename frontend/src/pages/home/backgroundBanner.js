5ca83402114c87ab30e4eb05baf9b21f8f918fee
WhynterLeDosFin <armel.ranaldi@gmail.com>
2022-04-18 18:30:23 +0200

    feat: adding the two first parts to homepage 🚧

---
+++ frontend/src/pages/home/backgroundBanner.js
@@ -0,0 +1,16 @@
+import './backgroundBanner.scss';
+
+export default function BackgroundBanner(props) {
+    return (
+        <div>
+            <img
+            className="backgroundBanner"
+            src={props.srcImg}
+            />
+            <p className={props.text1Class}>{props.text1}</p>
+            <p className={props.text2Class}>{props.text2}</p>
+            <p className={props.text3Class}>{props.text3}</p>
+            <p className={props.text4Class}>{props.text4}</p>
+        </div>
+    );
+}
--- frontend/src/pages/home/home.js
+++ frontend/src/pages/home/home.js
@@ -1,5 +1,27 @@
-//import './home.scss';
+import './home.scss';
+import BackgroundBanner from "./backgroundBanner";
 
 export default function Home() {
-    return <p>home works !</p>
+    return (
+            <div>
+                <BackgroundBanner
+                    srcImg="../../../banners/LAWIRA_FIRST_BANNER.png"
+                    text1="Choisissez la" text1Class="bannerText"
+                    text2="SÉCURITÉ" text2Class="bannerBoldText"
+                    text3="choisissez" text3Class="bannerText"
+                    text4="LAWIRA" text4Class="bannerBoldTextEnd"
+                />
+                <div className="blackBlock"></div>
+                <div className="first-part">
+                    <p className="paragraph">pour toutes vos données !</p>
+                </div>
+                <BackgroundBanner
+                    srcImg="../../../banners/LAWIRA_SECOND_BANNER.png"
+                    text1="Avec notre support USB" text1Class="leftBannerText"
+                    text2="LAWIRA" text2Class="leftBannerBoldText"
+                    text3="vos données n'appartiennent qu'à" text3Class="leftBannerText"
+                    text4="VOUS" text4Class="leftBannerBoldText"
+                />
+            </div>
+            )
 }
