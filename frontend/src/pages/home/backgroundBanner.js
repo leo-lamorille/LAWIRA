5755040be6a3761a07ff6f3959a03366a3162d6d
WhynterLeDosFin <armel.ranaldi@gmail.com>
2022-04-20 13:45:03 +0200

    feat: adding the light feature and begining the 3rd part 🚧

--- frontend/src/pages/home/backgroundBanner.js
+++ frontend/src/pages/home/backgroundBanner.js
@@ -1,11 +1,12 @@
 import './backgroundBanner.scss';
+import {css} from "@emotion/react";
 
 export default function BackgroundBanner(props) {
     return (
         <div>
             <img
-            className="backgroundBanner"
-            src={props.srcImg}
+                className="backgroundBanner"
+                src={props.srcImg}
             />
             <p className={props.text1Class}>{props.text1}</p>
             <p className={props.text2Class}>{props.text2}</p>
--- frontend/src/pages/home/home.js
+++ frontend/src/pages/home/home.js
@@ -1,7 +1,27 @@
+import {useState} from "react";
 import './home.scss';
 import BackgroundBanner from "./backgroundBanner";
+import {css} from "@emotion/react";
 
 export default function Home() {
+    const [light, setLight] = useState(1);
+    const [lightPic, setLightPic] = useState("../../../framePics/LIGHT_OFF_PIC.png");
+    const [lightHalo, setLightHalo] = useState({})
+    // <!-- src="../../../framePics/LIGHT_OFF_PIC.png" -->
+
+    function Light(event){
+        event.preventDefault();
+        if (light === 1){
+            setLight(0);
+            setLightPic("../../../framePics/LIGHT_ON_PIC.png");
+            setLightHalo({boxShadow: "23vw -17vw 40vw 6vw rgb(141 202 229 / 60%)"});
+        } else {
+            setLight(1);
+            setLightPic("../../../framePics/LIGHT_OFF_PIC.png");
+            setLightHalo({});
+        }
+    }
+
     return (
             <div>
                 <BackgroundBanner
@@ -20,8 +40,19 @@ export default function Home() {
                     text1="Avec notre support USB" text1Class="leftBannerText"
                     text2="LAWIRA" text2Class="leftBannerBoldText"
                     text3="vos données n'appartiennent qu'à" text3Class="leftBannerText"
-                    text4="VOUS" text4Class="leftBannerBoldText"
+                    text4="VOUS" text4Class="leftBannerBoldTextEnd"
                 />
+                <div className="second-part">
+                    <p className="lightWire">
+                        l
+                    </p>
+                    <div className="lightFrame">
+                        <a href="" onClick={ Light }><img className="lightPic" src={lightPic}/></a>
+                    </div>
+                    <div className="picFrame" style={lightHalo}>
+                        <img src="../../../framePics/FIRST_FRAME_PIC.png"/>
+                    </div>
+                </div>
             </div>
             )
 }
