f40cd3cfcb0001fd5bdcc79e5166226c4930c354
leo lamorille <lamorilleleo@gmail.com>
2022-05-03 11:18:32 +0200

    ✨: about page

--- frontend/src/pages/aboutUs/aboutUs.js
+++ frontend/src/pages/aboutUs/aboutUs.js
@@ -1,5 +1,43 @@
 import './aboutUs.scss';
+import {Avatar, AvatarGroup} from "@mui/material";
 
 export default function AboutUs() {
-    return <p>About us works !</p>
+    const styleAvatar = {
+        height: '200px',
+        width: '200px'
+    }
+    return (
+      <div className="aboutContainer">
+          <div className="title">
+              <p>QUI SOMMES NOUS ?</p>
+          </div>
+          <div className="content">
+              <p className="text">
+                  LAWIRA est une jeune start’up née à Lille composée
+                  de 3 jeunes ingénieurs fraîchement diplômés
+              </p>
+              <div className="avatarContainer">
+                  <div>
+                      <Avatar className="avatar" alt="Armel Photo" src="armel.png" style={styleAvatar}/>
+                      <p className="name">Armel RANALDI</p>
+                  </div>
+                  <div>
+                      <Avatar className="avatar" alt="Leo Photo" src="leo.png" style={styleAvatar}/>
+                      <p className="name">Léo LAMORILLE</p>
+                  </div>
+                  <div>
+                      <Avatar className="avatar" alt="Hugo Photo" src="hugo.png" style={styleAvatar}/>
+                      <p className="name">Hugo WIEDER</p>
+                  </div>
+              </div>
+              <div className="objectifContainer">
+                  <p>Je suis l'objectif de cette startup (nan je rigole aller faut le définir la)</p>
+              </div>
+              <div className="collabContainer">
+                  <p>En collaboration avec </p>
+                  <img className="imgJunia" src="junia.png" alt="junia photo"/>
+              </div>
+          </div>
+      </div>
+    );
 }
