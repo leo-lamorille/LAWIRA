b87b8363a752999f42ccc45fbbe3fb8b9f15b324
WhynterLeDosFin <armel.ranaldi@gmail.com>
2022-05-16 18:31:52 +0200

    feat: designing account page config and admin page basket section :art:

---
+++ frontend/src/pages/account/sectionConfig/sectionConfig.js
@@ -0,0 +1,49 @@
+import './sectionConfig.scss';
+import {useEffect, useState} from "react";
+import {Link} from "react-router-dom";
+
+export default function SectionConfig({options, name, deleteConfig, id, updateConfigURL}) {
+    const isAColor = (value, type) => {
+        return (
+            (type === "COLOR")
+                ? <div className="colordiv" style={{backgroundColor: value}}/>
+
+                : value
+        );
+    };
+    function deleteConfiguration(){
+        deleteConfig(id);
+    };
+    return (
+      <div className="config">
+          <div>
+                <div className="configTitle">
+                    <p>{name}</p>
+                </div>
+                <div className="imgCommand">
+                    <img src="/clef/twoClef.png" alt="usb clef"/>
+                </div>
+          </div>
+          <div className="options">
+                  {
+                      options.map(({
+                                       attributeId,
+                                       attributeName,
+                                       optionValue,
+                                       optionType
+                                   }) => {
+                          return (
+                              <div className={"value"} key={attributeId}>
+                                  <div className="text">
+                                      {attributeName} : {isAColor(optionValue, optionType)}
+                                  </div>
+                              </div>
+                          );
+                      })
+                  }
+            </div>
+          <Link to={updateConfigURL(options, id)}><button className="btn styledButton">Modifier</button></Link>
+          <button className="btn styledButton" onClick={deleteConfiguration}>Supprimer</button>
+      </div>
+    );
+}
